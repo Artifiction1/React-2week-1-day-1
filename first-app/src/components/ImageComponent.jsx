484280f6ec479bfff74d0af2624389dcfbbac327
Artifiction1 <angelus1@inbox.lv>
2022-06-20 17:09:34 +0300

    Update ImageComponent.jsx

--- first-app/src/components/ImageComponent.jsx
+++ first-app/src/components/ImageComponent.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import queen from './queen.png';
+
 class ImageComponent extends Component {
     render(){
         return <img src={this.props.src} alt={this.props.alt}/>
